5eacf78230e8e5cde82e8be9314d039de5b54db0
Mohsen <Mohsen@gmail.com>
2026-04-20 11:15:02 +0300

    Added site administration

--- Planteer/plant/admin.py
+++ Planteer/plant/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Plant, Comment
+
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'is_edible', 'created_at')
+    list_filter = ('category', 'is_edible')
+    search_fields = ('name', 'about', 'used_for')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plant', 'created_at')
+    list_filter = ('plant', 'created_at')
+    search_fields = ('name', 'comment')
+
 
 # Register your models here.
+admin.site.register(Plant, PlantAdmin)
+admin.site.register(Comment, CommentAdmin)
--- Planteer/plant/models.py
+++ Planteer/plant/models.py
@@ -17,9 +17,15 @@ class Plant(models.Model):
     is_edible = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class Comment(models.Model):
     plant =models.ForeignKey(Plant,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.plant.name}"
     
